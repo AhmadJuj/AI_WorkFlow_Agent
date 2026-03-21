f6e6a02c07c2d48d8f74f252db320c721841b24e
Ahmad Wasim Judge <sp23-bse-012@cuilahore.edu.pk>
2026-03-21 06:56:05 +0500

    Delete embed-chat-react directory

--- embed-chat-react/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import EmbedChat from "./embedchat.tsx";
-
-createRoot(document.getElementById("root")!).render(<EmbedChat />);
